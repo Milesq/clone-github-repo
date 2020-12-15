032cb2849d6f91483be49a6087c9ba5f3d6ea9d4
Milesq <wmilosz88@gmail.com>
2020-12-15 20:58:47 +0100

    refactor: momove swicthes logic to different function

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,34 @@ use {
     },
 };
 
+fn execute_switch(args: Vec<String>, actions: Vec<(&[&str], impl Fn())>) -> bool {
+    let is_switch_set = |switches: &[&str]| {
+        args.iter()
+            .any(|el| switches.iter().any(|switch| switch == el))
+    };
+
+    for (args, handler) in actions {
+        if is_switch_set(args) {
+            handler();
+            return true;
+        }
+    }
+
+    false
+}
+
+fn preparse_args(args: Vec<String>) -> bool {
+    let actions: Vec<(&[&str], fn())> = vec![
+        (&["-h", "--help"], || println!("{}", HELP_MSG)),
+        (&["-c", "--clean"], || {
+            std::fs::remove_file(dirs::home_dir().unwrap().join("./clone-cfg.bin")).unwrap();
+            println!("Clean");
+        }),
+    ];
+
+    execute_switch(args, actions)
+}
+
 fn main() {
     let mut c = AppData::new().unwrap();
     let user_name = c.get("user_name").map(String::from).unwrap_or_else(|| {
@@ -30,17 +58,7 @@ fn main() {
 
     let args: Vec<_> = env::args().collect();
 
-    let switch_is_set = |switches: &[&str]| {
-        args.iter()
-            .any(|el| switches.iter().any(|switch| switch == el))
-    };
-
-    if switch_is_set(&["-h", "--help"]) {
-        println!("{}", HELP_MSG);
-        return;
-    } else if switch_is_set(&["-c", "--clean"]) {
-        std::fs::remove_file(dirs::home_dir().unwrap().join("./clone-cfg.bin")).unwrap();
-        println!("Clean");
+    if preparse_args(args.clone()) {
         return;
     }
 
