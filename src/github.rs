34788957e6c01a6cd8413e4d4401f0972a7e29cd
Milesq <wmilosz88@gmail.com>
2020-12-25 14:11:57 +0100

    feat: return err when api didn't returns `data` nor `errors`

--- src/github.rs
+++ src/github.rs
@@ -1,22 +1,36 @@
-use {dialoguer::Select, serde_json::{Value, json}, gql::GraphqlClient};
+use {
+    dialoguer::Select,
+    gql::{GraphqlClient, GraphqlResult},
+    serde::Deserialize,
+    serde_json::{json, Value},
+};
 
 #[derive(Debug, Clone)]
 pub struct GHProfile(pub String);
 
 impl GHProfile {
     pub fn repos(&self) -> Option<Vec<String>> {
-        let mut gql = gql::GraphqlClient::new("https://api.github.com/graphql");
+        let mut gql = GraphqlClient::new("https://api.github.com/graphql");
         let get_repos_query = gql
-            .auth("80da5a3b6eeb85f66fc6111529fd01d73d012b27")
+            .auth("5a3f51740dca225ae7bf76f2e72956aaa8838136")
             .query(include_str!("./getRepos.gql"));
 
-        let data = get_repos_query.send(Some(json!({
-            "login": "Milesq"
-        })));
+        let data = get_repos_query
+            .send(Some(json!({
+                "login": "Milesq"
+            })))
+            .unwrap();
 
-        println!("{:#?}", data);
-
-        Some(vec!["".to_string(), "das".to_string()])
+        Some(
+            serde_json::from_value::<ReposResponse>(data)
+                .ok()?
+                .user
+                .repositories
+                .nodes
+                .iter()
+                .map(|el| el.name.clone())
+                .collect::<Vec<_>>(),
+        )
     }
 
     pub fn repo_exists(&self, name: &str) -> bool {
@@ -38,4 +52,23 @@ impl GHProfile {
             .flatten()
             .map(|choosen| choosen.as_str().to_string())
     }
+}
+
+#[derive(Deserialize, Debug)]
+struct ReposResponse {
+    user: User,
+}
+
+#[derive(Deserialize, Debug)]
+struct User {
+    repositories: Repo,
+}
+#[derive(Deserialize, Debug)]
+struct Repo {
+    nodes: Vec<Node>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Node {
+    name: String,
 }
